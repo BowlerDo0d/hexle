6bcf8021175c844ecb2214e0cecc6e5f92a7fcea
BowlerDo0d <smahony39@gmail.com>
2022-03-05 10:08:32 -0500

    updated prettier settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
+const fs = require('fs');
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier'
   ],
   env: {
-    "vue/setup-compiler-macros": true,
+    'vue/setup-compiler-macros': true
   },
+  rules: {
+    'prettier/prettier': ['warn', fs.readFileSync('.prettierrc')]
+  }
 };
