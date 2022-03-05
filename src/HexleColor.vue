ed2625bb5db209e76139913ca5cccf34e7f5e4d1
BowlerDo0d <smahony39@gmail.com>
2022-03-04 22:23:22 -0500

    Initial commit

---
+++ src/HexleColor.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      thing: 'hello'
+    };
+  }
+});
+</script>
+
+<template>
+  <h1>Hexle <span>Color</span></h1>
+
+  <div>Random color here</div>
+
+  <div>User guess here</div>
+
+  <div>Random {{ thing }}</div>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+// import App from './App.vue';
+import HexleColor from './HexleColor.vue';
+
+createApp(HexleColor).mount('#app');
